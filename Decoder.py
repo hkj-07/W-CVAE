de152e61c5c7c8d714694064239331e69a1af3c4
ALENAVENGER <chenalenavenger@gmail.com>
2020-12-22 21:32:54 +0800

    陈：修改decoder

--- Decoder.py
+++ Decoder.py
@@ -1,62 +1,44 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.utils.spectral_norm as spectral_norm
 
 """
 解码器
-输入：Z
-输出：
+输入：Z和标签
+输出：对应类别的x
 """
-
-class Decoder(nn.Module):
-    def __init__(self, img_size, img_channels, z_size):
-        super(DecoderBlock, self).__init__()
-
-        # transpose convolution to double the dimensions
-        self.conv = nn.ConvTranspose2d(in_channels=channel_in, out_channels=channel_out, kernel_size=5, padding=2,
-                                       stride=2, output_padding=1, bias=False)
-        self.bn = nn.BatchNorm2d(channel_out, momentum=0.9)
-
-    def forward(self, ten):
-        ten = self.conv(ten)
-        ten = self.bn(ten)
-        # ten = F.relu(ten, True)
-        ten = nn.LeakyReLU(ten, inplace=True)
-        return ten
 
         
 class Decoder(nn.Module):
-    def __init__(self, z_size, size, num_classes=10):
+    def __init__(self, img_size, img_channels, z_size, n_classes):
         super(Decoder, self).__init__()
 
-        # start from B * z_size
-        # concatenate one hot encoded class vector
-        self.fc = nn.Sequential(nn.Linear(in_features=(z_size + num_classes), out_features=(8 * 8 * size), bias=False),
-                                nn.BatchNorm1d(num_features=8 * 8 * size, momentum=0.9),
-                                nn.ReLU(True))
-        self.size = size
+        self.img_size = img_size
+        self.embed_z = spectral_norm(nn.Linear(z_size+n_classes, 7*7*256))
 
-        layers = [
-            DecoderBlock(channel_in=self.size, channel_out=self.size),
-            DecoderBlock(channel_in=self.size, channel_out=self.size // 2)
-        ]
+        def conv_block(in_channels, out_channels, bn=True, up=True):
+            layers = []
+            if up:
+                layers.append(nn.Upsample(scale_factor=2))
+            layers.append(spectral_norm(nn.Conv2d(in_channels, out_channels, 3, stride=1, padding=1)))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            if bn:
+                layers.append(nn.BatchNorm2d(out_channels, 0.8))
+            return layers
+        
+        self.model = nn.Sequential(
+            *conv_block(256, 128),
+            *conv_block(128, 64),
+            *conv_block(64, img_channels, up=False)
+        )
 
-        self.size = self.size // 2
-        layers.append(DecoderBlock(channel_in=self.size, channel_out=self.size // 4))
-        self.size = self.size // 4
-
-        # final conv to get 3 channels and tanh layer
-        layers.append(nn.Sequential(
-            nn.Conv2d(in_channels=self.size, out_channels=3, kernel_size=5, stride=1, padding=2),
-            nn.Tanh()
-        ))
-        self.conv = nn.Sequential(*layers)
-
-    def forward(self, ten, one_hot_classes):
-        ten_cat = torch.cat((one_hot_classes, ten), 1)
-        ten = self.fc(ten_cat)
-        ten = ten.view(len(ten), -1, 8, 8)
-        ten = self.conv(ten)
-        return ten
-
-    def __call__(self, *args, **kwargs):
-        return super(Decoder, self).__call__(*args, **kwargs)
+    def forward(self, z, label):
+        # 将z和label连起来
+        catted_z = torch.cat([z, label], dim=1)
+        # 映射到256*7*7的图片
+        conv_input = self.embed_z(catted_z)
+        conv_input = conv_input.view(-1, 256, self.img_size, self.img_size)
+        # 得到生成样本
+        conv_output = self.model(conv_input)
+        return conv_output
