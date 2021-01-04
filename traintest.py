5c8e8c6f5d7cfc6f22eb4a10bb46c294c42b72ae
hkj <1007692607@qq.com>
2021-01-04 22:02:29 +0800

    精准度测量修改

--- traintest.py
+++ traintest.py
@@ -342,8 +342,11 @@ if __name__ == '__main__':
 
             optimizer_classifier.step()
             total += target.size(0)
-            compares= torch.stack([predicts1,target],axis = 1)
-            for pre in compares:
+            # compares= torch.stack([predicts1,target],axis = 1)
+            predicts1=predicts1.view(-1,opt.img_size, opt.img_size).unsqueeze(1)
+            target = target.view(-1,opt.img_size, opt.img_size).unsqueeze(1)
+            conv_input = torch.cat([predicts1, target], dim=1)
+            for pre in conv_input:
                 if pre[0] == pre[1]:
                     running_correct=running_correct+1
             
