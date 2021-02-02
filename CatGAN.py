c6bdffae98ceefcaf2113f17925b562738bb3309
hkj <1007692607@qq.com>
2021-02-02 15:06:44 +0800

    修改loss曲线横坐标

--- CatGAN.py
+++ CatGAN.py
@@ -412,7 +412,7 @@ val_marginal_entropies_fake=[]
 for epoch in range(EPOCH):
     t0 = time.time()    
     print(f"\n=============== EPOCH {epoch+1} / {EPOCH} ===============\n")
-    batches_losses_tmp_G, batches_losses_tmp_D, conditional_entropies_real_tmp, marginal_entropies_real_tmp, cross_entropies_tmp, conditional_entropies_fake_tmp, marginal_entropies_fake_tmp=train_loop_fun1(train_data_loader, discriminator, generator, optimizer_G.detach(), optimizer_D.detach(), latent_size, TRAIN_BATCH_SIZE, device, λ)
+    batches_losses_tmp_G, batches_losses_tmp_D, conditional_entropies_real_tmp, marginal_entropies_real_tmp, cross_entropies_tmp, conditional_entropies_fake_tmp, marginal_entropies_fake_tmp=train_loop_fun1(train_data_loader, discriminator, generator, optimizer_G, optimizer_D, latent_size, TRAIN_BATCH_SIZE, device, λ)
     epoch_loss_D=np.mean(batches_losses_tmp_D)
     epoch_loss_G=np.mean(batches_losses_tmp_G)
     print(f"\n*** avg_Generator_loss : {epoch_loss_G:.2f}, avg_Discriminator_loss : {epoch_loss_D:.2f}, time : ~{(time.time()-t0)//60} min ({time.time()-t0:.2f} sec) ***\n")
@@ -424,8 +424,8 @@ for epoch in range(EPOCH):
     # val_acc.append(tmp_evaluate['accuracy'])
     # val_losses_D.append(val_losses_tmp_D)
     # val_losses_G.append(val_losses_tmp_G)
-    batches_losses_D.append(batches_losses_tmp_D)
-    batches_losses_G.append(batches_losses_tmp_G)
+    batches_losses_D.append(batches_losses_tmp_D.detach())
+    batches_losses_G.append(batches_losses_tmp_G.detach())
     
     # conditional_entropies_real.append(conditional_entropies_real_tmp)
     # marginal_entropies_real.append(marginal_entropies_real_tmp)
